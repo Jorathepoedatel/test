f2b40665c4498ee2c9abf92612bac237ff64216e
Dmitry <8772670177@my.lerna.by>
2026-05-13 01:11:52 +0300

    bugfix

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,33 @@
 import pytest
 from httpx import AsyncClient, ASGITransport
 
+from src.database import engine, Base, async_session
 from src.main import app, get_db
 
 
+@pytest.fixture(autouse=True)
+async def prepare_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+    yield
+
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+
+
+async def override_get_db():
+    async with async_session() as session:
+        yield session
+
 @pytest.fixture
 async def client():
+    app.dependency_overrides[get_db] = override_get_db
+
     async with AsyncClient(
         transport=ASGITransport(app=app),
         base_url="http://test",
     ) as ac:
         yield ac
+
+    app.dependency_overrides.clear()
